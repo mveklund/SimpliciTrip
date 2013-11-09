0e81f1f59600a1a58176e40db863c841cbe5f90a
Veronica Eklund <mveklund@gmail.com>
2013-11-08 19:36:33 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpliciTrip::Application.config.secret_token = '651742051edf65138a7f7ec5da9be8cb000f75dfa76ea02d7c2f3ba05d3e0dadcf454c589359b0620a44a82dc2eb5eecaa6a0ef37be6e4a752c7c321dc4d2d8c'
